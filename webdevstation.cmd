fcbb1205072b6e4278e25a250834e34f7a757f53
Francois X <kfx@free.fr>
2018-09-19 17:46:02 +0200

    Update webdevstation.cmd

--- webdevstation.cmd
+++ webdevstation.cmd
@@ -9,6 +9,7 @@ choco install -y -f chocolateygui
 choco install -y -f composer
 choco install -y -f curl
 choco install -y -f ditto.install
+choco install -y -f docker-for-windows
 choco install -y -f dotnet4.7
 choco install -y -f git.install
 choco install -y -f greenshot
