223c6d7531bbff2eee73a26b93f76f88018e73a8
Francois X <kfx@free.fr>
2018-09-19 14:25:43 +0200

    Update webdevstation.cmd

--- webdevstation.cmd
+++ webdevstation.cmd
@@ -1,26 +1,34 @@
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) 
-choco install 7zip.install
-choco install agentransack
-choco install beyondcompare
-choco install chocolateygui
-choco install colorPic
-choco install composer
-choco install curl
-choco install ditto.install
-choco install dotnet4.7
-choco install git.install
-choco install greenshot
-choco install iperf3
-choco install MobaXTerm
-choco install nircmd
-choco install nodejs.install
-choco install notepad2
-choco install php
-choco install powerShell
-choco install sourcetree
-choco install sumatrapdf.install
-choco install sysinternals
-choco install visualstudiocode
-choco install vlc
-choco install wget
-choco install winscp
+choco install -y -f 7zip.install
+choco install -y -f agentransack
+choco install -y -f azure-cli
+choco install -y -f azurepowershell
+choco install -y -f microsoftazurestorageexplorer
+choco install -y -f beyondcompare
+choco install -y -f chocolateygui
+choco install -y -f colorPic
+choco install -y -f composer
+choco install -y -f curl
+choco install -y -f ditto.install
+choco install -y -f dotnet4.7
+choco install -y -f git.install
+choco install -y -f greenshot
+choco install -y -f iperf3
+choco install -y -f markdownmonster
+choco install -y -f MobaXTerm
+choco install -y -f mysql
+choco install -y -f mysql.workbench
+choco install -y -f nircmd
+choco install -y -f nodejs.install
+choco install -y -f notepad2
+choco install -y -f pgadmin4
+choco install -y -f php
+choco install -y -f postgresql
+choco install -y -f powerShell
+choco install -y -f sourcetree
+choco install -y -f sumatrapdf.install
+choco install -y -f sysinternals
+choco install -y -f visualstudiocode
+choco install -y -f vlc
+choco install -y -f wget
+choco install -y -f winscp
